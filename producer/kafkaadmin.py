1bc0bae00339ea72990ff5d323abe393652b2592
Thierry Brands <thierrybrands@hotmail.com>
2024-12-04 11:08:38 +0100

    updated streaming files

--- producer/kafkaadmin.py
+++ producer/kafkaadmin.py
@@ -10,7 +10,7 @@ def create_topics(admin, topic_list):
 
 
 if __name__ == '__main__':
-    admin_client = KafkaAdminClient(bootstrap_servers="34.123.5.62:9092",
+    admin_client = KafkaAdminClient(bootstrap_servers="34.31.254.144:9092",
                                     client_id='Assignment2')  # use your VM's external IP Here!
     delete_topics(admin_client)
     topic_list = [NewTopic(name="movie_reviews", num_partitions=1, replication_factor=1)]
--- producer/producer.py
+++ producer/producer.py
@@ -1,33 +1,31 @@
 from kafka import KafkaProducer
 import json
+from tqdm import tqdm  # Import tqdm for progress tracking
 
 def kafka_python_producer_sync(producer, msg, topic):
     producer.send(topic, bytes(msg, encoding='utf-8'))
-    #print("Sending " + msg)
+    # print("Sending " + msg)
     producer.flush(timeout=60)
-
 
 def success(metadata):
     print(metadata.topic)
 
-
 def error(exception):
     print(exception)
-
 
 def kafka_python_producer_async(producer, msg, topic):
     producer.send(topic, bytes(msg, encoding='utf-8')).add_callback(success).add_errback(error)
     producer.flush()
 
-
 if __name__ == '__main__':
-    producer = KafkaProducer(bootstrap_servers='34.123.5.62:9092')  # use your VM's external IP Here!
+    producer = KafkaProducer(bootstrap_servers='34.31.254.144:9092')  # use your VM's external IP Here!
     # value_serializer = lambda v: json.dumps(v).encode('utf-8')
-    path = "C:/Users/thier/OneDrive/Documenten/JADS/Year 2/Data Engineering/data-engineering-2/data/imdb_Movie_Reviews_short.json"
-    with open(path) as f:   # change this path to the path in your laptop
+    path = "C:/Users/thier/OneDrive/Documenten/JADS/Year 2/Data Engineering/data-engineering-2/data/imdb_Movie_Reviews.json"
+    
+    with open(path) as f:  # change this path to the path on your laptop
         json_data = json.load(f)
 
-    # Process each JSON object in the file
-    for data in json_data:
+    # Process each JSON object in the file with a tqdm progress bar
+    for data in tqdm(json_data, desc="Sending data to Kafka"):
         kafka_python_producer_sync(producer, json.dumps(data), 'movie_reviews')
     f.close()
